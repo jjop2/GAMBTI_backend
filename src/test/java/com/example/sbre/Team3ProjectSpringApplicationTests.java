867ab114791eb161c54106a88eb0b2a313f81587
jjop2 <jngsuh616@gmail.com>
2025-08-26 11:45:47 +0900

    초기세팅

---
+++ src/test/java/com/example/sbre/Team3ProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team3ProjectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
