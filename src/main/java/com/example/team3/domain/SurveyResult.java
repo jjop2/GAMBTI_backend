e2205a2d43916faee35756242d6bcbefaefa93b0
guswlsl1216 <guswls023@naver.com>
2025-09-01 18:50:09 +0900

    Feat: get 요청기능 구현및테스트결과저장업데이트구햔

--- src/main/java/com/example/team3/domain/SurveyResult.java
+++ src/main/java/com/example/team3/domain/SurveyResult.java
@@ -1,12 +1,13 @@
 package com.example.team3.domain;
 
 import jakarta.persistence.Entity;
-
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -37,8 +38,8 @@ public class SurveyResult {
 
 	private String combinationGenre;
 	
-	@ManyToOne
-	@JoinColumn(name = "user_id")
+	@OneToOne(fetch = FetchType.EAGER)  // 유저 정보 바로 바로 불러올 수 있도록
+	@JoinColumn(name = "user_id", unique = true)  // DB에 user_Id 중복으로 들어가지 않게 unique 설정 
 	private User username;
 	
 }
--- src/main/java/com/example/team3/service/SurveyResultService.java
+++ src/main/java/com/example/team3/service/SurveyResultService.java
@@ -1,7 +1,13 @@
 package com.example.team3.service;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
+import com.example.team3.DTO.SurveyResultRequestDTO;
+import com.example.team3.DTO.SurveyResultResponseDTO;
 import com.example.team3.domain.Genre;
 import com.example.team3.domain.SurveyResult;
 import com.example.team3.domain.User;
@@ -16,21 +22,57 @@ import lombok.RequiredArgsConstructor;
 public class SurveyResultService {
 
 	public final SurveyResultRepository surveyResultRepository;
-	
+
 	public final GenreRepository genreRepository;
 	public final UserRepository userRepository;
+
+
+	// 유저가 새롭게 테스트할 경우 기존 데이터 지우고 업데이트로 변경 
+	// 지우고 업데이트 메서드 명 수정했습니다! 
+	public void saveOrUpdateSurveyResult(SurveyResultRequestDTO request, User user) {
+
+		Genre g1 = genreRepository.findById(request.getPreferGenre1Id()).orElseThrow();
+		Genre g2 = genreRepository.findById(request.getPreferGenre2Id()).orElseThrow();
 	
-	public void saveSurveyResult (SurveyResult surveyResult) { 
+												
+		SurveyResult existingResult = surveyResultRepository.findByUsername(user).orElse(null);
+		SurveyResult result;
+
+		if (existingResult != null)	{
+			result = existingResult;
+		} else { 
+			result = new SurveyResult();
+		}
+		   result.setAge(request.getAge());
+		   result.setGender(request.getGender());
+		   result.setCombinationGenre(request.getCombinationGenre());
+		   result.setPreferGenre1(g1);
+		   result.setPreferGenre2(g2);
+		   result.setUsername(user);
+		   
+		   System.out.println("세이브하기 전 값 셋팅"+ result);
+		   surveyResultRepository.save(result);
+
+	}
+
+	public List<SurveyResultResponseDTO> getSurveyResultByUser(User user) {
+		List<SurveyResult> results = surveyResultRepository.findAllByUsername(user); 
+
+		// surveyResult List로 만들어서 // user 객체 받아서 username으로 모든 데이터 조회 
 		
-		 	Genre g1 = genreRepository.findById(surveyResult.getPreferGenre1().getId()).orElseThrow();
-		    Genre g2 = genreRepository.findById(surveyResult.getPreferGenre2().getId()).orElseThrow();
-		    User user = userRepository.findById(surveyResult.getUsername().getId()).orElseThrow();
-
-		    surveyResult.setPreferGenre1(g1);
-		    surveyResult.setPreferGenre2(g2);
-		    surveyResult.setUsername(user);
-
-		    surveyResultRepository.save(surveyResult);
+		
+		// DTO 형식에 맞게 다시 한번 => 패키징하고 => 리스트로 만들어서 리턴! 
+		return results.stream()
+					  .map(r -> new SurveyResultResponseDTO(
+					  r.getAge(),
+					  r.getGender(),
+					  r.getPreferGenre1().getId(),
+					  r.getPreferGenre2().getId(),
+					  r.getCombinationGenre(),
+					  r.getUsername().getId()
+					  ))
+					  .collect(Collectors.toList());
+							  
+				
 	}
 }
-
